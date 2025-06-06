236df24cc5673a8bea2dc988140ec1de973a1ffa
siddesh <siddhesh112004@gmail.com>
2025-06-06 13:08:27 +0530

    frontedn check

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,14 @@ import React from 'react'
 function App() {
   return (
     <>
+    <button className="btn btn-neutral">Neutral</button>
+<button className="btn btn-primary">Primary</button>
+<button className="btn btn-secondary">Secondary</button>
+<button className="btn btn-accent">Accent</button>
+<button className="btn btn-info">Info</button>
+<button className="btn btn-success">Success</button>
+<button className="btn btn-warning">Warning</button>
+<button className="btn btn-error">Error</button>
     </>
   )
 }
